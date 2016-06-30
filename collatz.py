44693334a15c7ab48d3fc5ee0041442115e2478c
shr612 <onlyshrey@gmail.com>
2016-06-30 17:07:35 +0530

    Update collatz.py

--- collatz.py
+++ collatz.py
@@ -1,10 +1,4 @@
 ## The Collatz Conjecture (http://en.wikipedia.org/wiki/Collatz_conjecture)
-## Take any natural number n.
-## If n is even, divide it by 2.
-## If n is odd multiply it by 3 and add 1.
-## Repeat the process indefinitely.
-## The conjecture is that, no matter what number you start with, you will always eventually reach 1.
-## The property has also been called oneness.
 
 import string
 
